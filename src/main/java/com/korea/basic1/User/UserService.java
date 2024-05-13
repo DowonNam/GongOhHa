479e247ec0b03c85d0f6eab05fb209e2d675d1a4
Dowon Nam <dwnam0114@gmail.com>
2024-05-13 09:57:33 +0900

    add the sighn up function

--- src/main/java/com/korea/basic1/User/UserService.java
+++ src/main/java/com/korea/basic1/User/UserService.java
@@ -1,9 +1,23 @@
 package com.korea.basic1.User;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password, String usernickname) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setUsernickname(usernickname);
+        this.userRepository.save(user);
+        return user;
+    }
 }
